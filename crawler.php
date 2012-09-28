4e0beb3421f6ef14c88894b16755f8f033cfb996
philipp <vanillawhitey@gmail.com>
2012-09-28 16:12:32 +0200

    passwordtest

--- crawler.php
+++ crawler.php
@@ -11,45 +11,48 @@
 	if(stripos($html->plaintext, 'Keine Lehrveranstaltungen gefunden!') === false){
 
 
-	$spantext = $html->find('span[class=text]');
+		$spantext = $html->find('span[class=text]');
 
-	$id =  $spantext[0]->children(0)->children(1)->children(0)->plaintext;
-	$type = $spantext[0]->children(0)->children(1)->children(1)->plaintext;
-	$title = $spantext[0]->children(0)->children(1)->children(3)->children(0)->plaintext;
-	$lecturer = $spantext[0]->children(2)->children(0)->children(1);
-	
-
-	if($spantext[0]->children(2)->children(1)->children(0)->plaintext == 'Planpunkte Bachelor'){
-
-	$subject = $spantext[0]->children(2)->children(1)->children(1)->plaintext;
-	}
-	$sst = $spantext[0]->children(2)->children(2)->children(1);
-	$language = $spantext[0]->children(2)->children(3)->children(1);
-
-	echo $id.'</br>';
-	echo $type.'</br>';
-	echo $title.'</br>';
-
-	foreach($lecturer->find('a') as $l){
-
-		echo $l->plaintext .'</br>';
-	}
-
-	foreach($subject->find('a') as $s){
-
-		echo $s->plaintext .'</br>';
-	}
-
-	echo $sst.'</br>';
-	echo $language.'</br>';
-
-	echo '</br></br>';
-
-	}
-	else{
-
+		$id =  $spantext[0]->children(0)->children(1)->children(0)->plaintext;
+		$type = $spantext[0]->children(0)->children(1)->children(1)->plaintext;
+		$title = $spantext[0]->children(0)->children(1)->children(3)->children(0)->plaintext;
+		$lecturer = $spantext[0]->children(2)->children(0)->children(1);
 		
-	}
+		// password test
+
+		if($spantext[0]->children(2)->children(1)->children(0)->plaintext == 'Planpunkte Bachelor'){
+
+			$subject = $spantext[0]->children(2)->children(1)->children(1)->plaintext;
+		}
+
+		$subject = $spantext[0]->children(2)->children(1)->children(1)
+		$sst = $spantext[0]->children(2)->children(2)->children(1);
+		$language = $spantext[0]->children(2)->children(3)->children(1);
+
+		echo $id.'</br>';
+		echo $type.'</br>';
+		echo $title.'</br>';
+
+		foreach($lecturer->find('a') as $l){
+
+			echo $l->plaintext .'</br>';
+		}
+
+		foreach($subject->find('a') as $s){
+
+			echo $s->plaintext .'</br>';
+		}
+
+		echo $sst.'</br>';
+		echo $language.'</br>';
+
+		echo '</br></br>';
+
+		}
+		else{
+
+			
+		}
 	
 
 
