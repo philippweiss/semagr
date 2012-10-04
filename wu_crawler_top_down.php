67589a79eb929ce4b0ad1331c0554d5ee6d70d85
philipp <vanillawhitey@gmail.com>
2012-10-05 01:03:16 +0200

    mathe statistik unsolved

--- wu_crawler_top_down.php
+++ wu_crawler_top_down.php
@@ -10,6 +10,7 @@
 	$kursanzahl = 0;
 	$uni = 'Wirtschaftsuniversität Wien';
 	mysql_query('insert into uni (title) values ("'.$uni.'")');
+	$uni_id = mysql_insert_id();
 
 	$html = file_get_html('http://vvz.wu.ac.at/cgi-bin/vvz.pl?LV=3;L2=38107;L3=38079;S=12W;LANG=DE');
 
@@ -18,7 +19,8 @@
 		echo $studienrichtung->children(0)->plaintext.'</br>';
 		$currentstudienrichtung = $studienrichtung->children(0)->plaintext;
 		mysql_query('insert into studienrichtung (title) values("'.$currentstudienrichtung.'")');
-		mysql_query('insert into uni_studienrichtung (uni_title,studienrichtung_title) values ("'.$uni.'","'.$currentstudienrichtung.'")');
+		$currentstudienrichtung_id = mysql_insert_id();
+		mysql_query('insert into uni_studienrichtung (uni_id,studienrichtung_id) values ("'.$uni_id.'","'.$currentstudienrichtung_id.'")');
 		$link = $studienrichtung->children(0)->href;
 		$html = file_get_html('http://vvz.wu.ac.at'.$link);
 
@@ -27,9 +29,8 @@
 			echo $studienzweig->children(0)->plaintext.'</br>';
 			$currentstudienzweig = $studienzweig->children(0)->plaintext;
 			mysql_query('insert into studienzweig (title) values("'.$currentstudienzweig.'")');
-			mysql_query('insert into studienrichtung_studienzweig (studienrichtung_title,studienzweig_title) values ("'.$currentstudienrichtung.'","'.$currentstudienzweig.'")');
-			
-
+			$currentstudienzweig_id = mysql_insert_id();
+			mysql_query('insert into studienrichtung_studienzweig (studienrichtung_id,studienzweig_id) values ("'.$currentstudienrichtung_id.'","'.$currentstudienzweig_id.'")');
 			$link = $studienzweig->children(0)->href;
 			$html = file_get_html('http://vvz.wu.ac.at'.$link);
 
@@ -38,86 +39,101 @@
 				echo $studienfach->children(0)->plaintext.'</br>';
 				$currentstudienfach = $studienfach->children(0)->plaintext;
 				mysql_query('insert into studienfach (title) values("'.$currentstudienfach.'")');
-				mysql_query('insert into studienfach_studienzweig (studienfach_title,studienzweig_title) values ("'.$currentstudienfach.'","'.$currentstudienzweig.'")');
+				$currentstudienfach_id = mysql_insert_id();
+				mysql_query('insert into studienfach_studienzweig (studienfach_id,studienzweig_id) values ("'.$currentstudienfach_id.'","'.$currentstudienzweig_id.'")');
 				$link = $studienfach->children(0)->href;
 				$html = file_get_html('http://vvz.wu.ac.at'.$link);
+/*
+				if($html->find('li[class=pfeilblaulink]')){ //planpunkte vorhanden
 
-				foreach($html->find('li[class=pfeilblaulink]') as $studienplanpunkt){
-
-					echo $studienplanpunkt->children(0)->plaintext.'</br>';
-					$currentstudienplanpunkt = $studienplanpunkt->children(0)->plaintext;
-					mysql_query('insert into studienplanpunkt (title) values("'.$currentstudienplanpunkt.'")');
+					$needle = 'li[class=pfeilblaulink]';
 					$link = $studienplanpunkt->children(0)->href;
 					$html = file_get_html('http://vvz.wu.ac.at'.$link);
+				}
+				else if($html->find('div[class=vvzh5]')){ //keine planpunkte vorhanden
 
-					foreach($html->find('td[class=vvzc1]') as $kurs){
+					$needle = 'div[class=vvzh5]';
+				}
+*/					foreach($html->find('li[class=pfeilblaulink]') as $studienplanpunkt){
 
-						//echo $kurs->children(0)->plaintext.'</br>';
-						$kursanzahl += 1;
-						$link = $kurs->children(0)->href;
+						echo $studienplanpunkt->plaintext.'</br>';
+						$currentstudienplanpunkt = $studienplanpunkt->children(0)->plaintext;
+						
+						mysql_query('insert into studienplanpunkt (title) values("'.$currentstudienplanpunkt.'")');
+						$currentstudienplanpunkt_id = mysql_insert_id();
+						mysql_query('insert into studienplanpunkt_studienfach (studienplanpunkt_id,studienfach_id) values ("'.$currentstudienplanpunkt_id.'","'.$currentstudienfach_id.'")');
+						$link = $studienplanpunkt->children(0)->href;
 						$html = file_get_html('http://vvz.wu.ac.at'.$link);
 
-						$spantext = $html->find('span[class=text]');
-				
-						$id =  $spantext[0]->children(0)->children(1)->children(0)->plaintext;
-						$type = $spantext[0]->children(0)->children(1)->children(1)->plaintext;
-						$title = $spantext[0]->children(0)->children(1)->children(3)->children(0)->plaintext;
-						$table2 = $spantext[0]->children(2);
-				
-						foreach($table2->find('td') as $cell){
 
-							if($cell->plaintext == 'LV-Leiter/in'){
+						foreach($html->find('td[class=vvzc1]') as $kurs){
 
-								$neighbour = $cell->next_sibling();
-								$lecturer = $neighbour;
-							}
-							if($cell->plaintext == 'Planpunkte Bachelor'){
+							//echo $kurs->children(0)->plaintext.'</br>';
+							$kursanzahl += 1;
+							$link = $kurs->children(0)->href;
+							$html = file_get_html('http://vvz.wu.ac.at'.$link);
 
-									$neighbour = $cell->next_sibling();
-									$subject = $neighbour;
-							}
-							if($cell->plaintext == 'Semesterstunden'){
+							$spantext = $html->find('span[class=text]');
+					
+							$id =  $spantext[0]->children(0)->children(1)->children(0)->plaintext;
+							$type = $spantext[0]->children(0)->children(1)->children(1)->plaintext;
+							$title = $spantext[0]->children(0)->children(1)->children(3)->children(0)->plaintext;
+							$table2 = $spantext[0]->children(2);
+					
+							foreach($table2->find('td') as $cell){
+
+								if($cell->plaintext == 'LV-Leiter/in'){
 
 									$neighbour = $cell->next_sibling();
-									$sst = $neighbour->plaintext;
+									$lecturer = $neighbour;
+								}
+								if($cell->plaintext == 'Planpunkte Bachelor'){
+
+										$neighbour = $cell->next_sibling();
+										$subject = $neighbour;
+								}
+								if($cell->plaintext == 'Semesterstunden'){
+
+										$neighbour = $cell->next_sibling();
+										$sst = $neighbour->plaintext;
+								}
+								if($cell->plaintext == 'Unterrichtssprache'){
+
+										$neighbour = $cell->next_sibling();
+										$language = $neighbour->plaintext;
+								}
 							}
-							if($cell->plaintext == 'Unterrichtssprache'){
 
-									$neighbour = $cell->next_sibling();
-									$language = $neighbour->plaintext;
-							}
-						}
+							echo $id.'</br>';
+							echo $type.'</br>';
+							echo $title.'</br>';
 
-						echo $id.'</br>';
-						echo $type.'</br>';
-						echo $title.'</br>';
-
-						if(isset($lecturer)){
-							
-							foreach($lecturer->find('a') as $l){
-
-								echo $l->plaintext.'</br>';
-							}
-						}
-
-						if(isset($subject)){
+							if(isset($lecturer)){
 								
-							foreach($subject->find('a') as $s){
+								foreach($lecturer->find('a') as $l){
 
-								echo $s->plaintext.'</br>';
+									echo $l->plaintext.'</br>';
+								}
 							}
-						}
-						if(isset($sst)){
 
-							echo $sst.'</br>';
-						}
-						if(isset($language)){
+							if(isset($subject)){
+									
+								foreach($subject->find('a') as $s){
 
-							echo $language.'</br>';
-						}
+									echo $s->plaintext.'</br>';
+								}
+							}
+							if(isset($sst)){
 
+								echo $sst.'</br>';
+							}
+							if(isset($language)){
+
+								echo $language.'</br>';
+							}
+
+						}
 					}
-				}
 			}
 		}
 	}
